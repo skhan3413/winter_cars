1a61e466bf1a6f7da60a44afec0718eda988329a
skhan3413 <skhan3413@yahoo.com>
2023-06-25 13:17:23 -0500

    We added the SQL file

---
+++ Project 1 wintercars.sql
@@ -0,0 +1,15 @@
+CREATE TABLE autos (
+model_name VARCHAR(50),
+automaker_name VARCHAR(50)
+);
+INSERT INTO autos (
+model_name,
+automaker_name)
+VALUES
+('Eclipse', 'Mitsubishi'),
+('Outlander', 'Mitsubishi'),
+('Hyundaii30', 'Hyundai'),
+('Ram1500', 'Dodge'),
+('FordF150', 'Ford'),
+('BMW335i', 'BMW'),
+('BMW330i', 'BMW');
